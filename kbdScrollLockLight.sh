624a881ce97c081728a038add29c77c325c0864a
Deyan Denev <deiandenev@gmail.com>
2025-03-07 14:26:00 +0200

    Update kbdScrollLockLight.sh
    More a simpler way to activate the keyboard light, removing 3 lines of code for more simplicity.

--- kbdScrollLockLight.sh
+++ kbdScrollLockLight.sh
@@ -1,11 +1,7 @@
 #1. Install brightnessctl and using it in BASH
 #Work under Arch
-#!/bin/bash
-kbd=`brightnessctl --list | grep scrolllock | cut -d ' ' -f 2`
-prefix="'"
-suffix="'"
-string=${kbd}
-lightON=${string#"$prefix"}
-lightON=${lightON%"$suffix"}
-echo `sleep 5 && brightnessctl --device="${lightON}" set 1`
+#!/bin/env bash
+kbd=`ls /sys/class/leds/ | grep scrolllock`
+echo `sleep 5 && brightnessctl --device="${kbd}" set 1`
+
 
