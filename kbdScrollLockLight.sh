e68f5561bca056d32f018cf85013abf1dd0ee5d7
Deyan Denev <deiandenev@gmail.com>
2025-03-07 14:48:32 +0200

    Update kbdScrollLockLight.sh

--- kbdScrollLockLight.sh
+++ kbdScrollLockLight.sh
@@ -1,6 +1,6 @@
-#1. Install brightnessctl and use it in BASH.
+#1. Install brightnessctl and use it in BASH.#Work under Arch
 #It's needed to activate "Map Scroll Lock to mod3" in KDE 6 keyboard options to eliminate conflicts when the user presses Caps Lock.
-#Work under Arch
+
 
 #!/bin/env bash
 kbd=`ls /sys/class/leds/ | grep scrolllock`
